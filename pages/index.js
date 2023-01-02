a34ca8193070a94e40f0a0014c3d1b787aec0f63
MichaelScovell <michael.scovell@cevo.com.au>
2023-01-02 13:45:51 +1100

    style updates

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ export default function Home() {
             }
           }}>Search</button>
         </div>
-        <div>
+        <div className={styles.cardcontainer}>
           {
             NFTs.length && NFTs.map(nft => {
               return (
